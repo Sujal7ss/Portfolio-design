4d1eb46886346b40e26686958a0130fed9af8368
Sujal7ss <104003078+Sujal7ss@users.noreply.github.com>
2023-08-05 11:07:38 +0530

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.querySelector(".navbar a").addEventListener("click", function (e) {
+    document.querySelector(".navbar a").style.display = "block";
+})
+
+document.querySelector("little").innerText = "Frontend designer";
